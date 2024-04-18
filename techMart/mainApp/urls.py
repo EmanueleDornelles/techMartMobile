3c9eaf73e120113ac727b4cbc81b5e62b3afdf33
Emanuele Dornelles <dornelles.mb@gmail.com>
2024-04-17 21:21:08 -0300

    correcoes

--- techMart/mainApp/urls.py
+++ techMart/mainApp/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('<int:pk>/',DetailProdutos.as_view()),
     path('',ListProdutos.as_view()),
-    path('create',CreateProdutos.as_view()),
-    path('delete<int:pk>',DeleteProdutos.as_view())
+    path('create/',CreateProdutos.as_view()),
+    path('delete<int:pk>/',DeleteProdutos.as_view())
 ]
